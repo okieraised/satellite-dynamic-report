d128b6c585dc8c7560a528d50b7205add6bf33bd
okieraised <phamtri2895@gmail.com>
2023-06-12 15:29:01 +0700

    added example script

--- src/geospatial/geotiff.py
+++ src/geospatial/geotiff.py
@@ -8,6 +8,8 @@ from rasterio.coords import BoundingBox
 from rasterio import windows
 from rasterio import warp
 from rasterio import mask
+from rasterio.warp import reproject, Resampling, calculate_default_transform
+
 
 import geopandas as gpd
 
@@ -60,20 +62,67 @@ with rio.open('../data/tiff/2022-07-30.tif') as src:
 
 
 if __name__ == "__main__":
-    # with gpd.read_file('../data/tiff/2022-07-30.tif') as src:
-    #     elevation = src.read()
-    #
-    #     print(elevation)
+
+    dst_crs = "EPSG:4326"
 
     with rio.open('../data/tiff/2022-07-30.tif') as src:
         elevation = src.read(1)
         print(elevation)
 
-        input_crs = src.crs
-        input_gt = src.transform
+        px_vals = []
 
-        print(f"input_crs: {input_crs}")
-        print(f"input_gt: {input_gt}")
+        for x in range(elevation.shape[0]):
+            for y in range(elevation.shape[1]):
+                px_vals.append({'x': x,
+                                'y': y,
+                                'value': elevation[x, y]})
+
+        print(px_vals)
+
+    # with rio.open('../data/tiff/2022-07-30.tif') as src:
+    #     elevation = src.read(1)
+    #     print(elevation)
+    #
+    #     input_crs = src.crs
+    #     input_gt = src.transform
+    #
+    #     print(f"input_crs: {input_crs}")
+    #     print(f"input_gt: {input_gt}")
+    #
+    #     src_transform = src.transform
+    #
+    #     # calculate the transform matrix for the output
+    #     dst_transform, width, height = calculate_default_transform(
+    #         src.crs,
+    #         dst_crs,
+    #         src.width,
+    #         src.height,
+    #         *src.bounds,  # unpacks outer boundaries (left, bottom, right, top)
+    #     )
+    #
+    #     # set properties for output
+    #     dst_kwargs = src.meta.copy()
+    #     dst_kwargs.update(
+    #         {
+    #             "crs": dst_crs,
+    #             "transform": dst_transform,
+    #             "width": width,
+    #             "height": height,
+    #             "nodata": 0,  # replace 0 with np.nan
+    #         }
+    #     )
+    #
+    #     with rio.open("../data/tiff/2022-07-30-2.tif", "w", **dst_kwargs) as dst:
+    #         for i in range(1, src.count + 1):
+    #             reproject(
+    #                 source=rio.band(src, i),
+    #                 destination=rio.band(dst, i),
+    #                 src_transform=src.transform,
+    #                 src_crs=src.crs,
+    #                 dst_transform=dst_transform,
+    #                 dst_crs=dst_crs,
+    #                 resampling=Resampling.nearest,
+    #             )
 
 
 
--- src/sample/sample.py
+++ src/sample/sample.py
@@ -9,16 +9,37 @@ app = Dash()
 #            style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block"}),
 # ])
 GEOTIFF_ID = "geotiff-id"
+color_domain = dict(domainMin=0.1, domainMax=2.0, colorscale=['white', 'orange', 'red'])
+# dl.Colorbar(width=200, height=20, min=0.00001, max=0.15, position="topleft", tickDecimals=2, unit=" ",
+#                        colorscale=['white', 'orange', 'red'], style={"color":"white"})],
+#                        id='raster2',center=(-16.394, -62.024), zoom=13, style={'width': '100%', 'height': '92.8vh'})
+# color_domain = dict(domainMin=1392, domainMax=1550, colorscale=['white', 'orange', 'red'])
+
 app.layout = html.Div([
+
     dl.Map(
-        [
-            dl.TileLayer(),
-            # dl.WMSTileLayer(url="https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi",
-            #                 layers="nexrad-n0r-900913", format="image/png", transparent=True),
-            dl.GeoTIFFOverlay(id=GEOTIFF_ID, band=1, url='/Users/tripham/Desktop/satellite-dynamic-report/src/data/tiff/2022-07-30.tif', interactive=True)
-        ],
-        zoom=4,
-        style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block"}),
+        style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block"},
+       zoom=5,
+       children=[
+           dl.TileLayer(),
+           dl.GeoTIFFOverlay(id=GEOTIFF_ID, interactive=True,
+                             url="/Users/tripham/Desktop/satellite-dynamic-report/src/data/tiff/2022-07-30-2.tif",
+                             band=2, opacity=0.8,
+                             **color_domain),
+           dl.Colorbar(width=200, height=20, min=0.1, max=2.0, position="topleft", tickDecimals=2, unit=" ",
+                       colorscale=['white', 'orange', 'red'], style={"color": "white"}),
+       ]),
+    # dl.Map(
+    #     [
+    #         dl.TileLayer(),
+    #         # dl.WMSTileLayer(url="https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi",
+    #         #                 layers="nexrad-n0r-900913", format="image/png", transparent=True),
+    #         dl.GeoTIFFOverlay(id=GEOTIFF_ID, opacity=0.8, band=1,
+    #                           url='/Users/tripham/Desktop/satellite-dynamic-report/src/data/tiff/tz850.tiff', # 2022-07-30-2
+    #                           interactive=True, **color_domain)
+    #     ],
+    #     zoom=4,
+    #     style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block"}),
 ])
 
 if __name__ == '__main__':
